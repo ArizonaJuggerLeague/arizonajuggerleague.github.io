e8cfdc0f07b8302c622f232255b4b1c1fc63bf1b
adrian <aciotinga@gmail.com>
2025-03-16 17:40:29 -0700

    removed stuff

--- zserve.bat
+++
@@ -1,1 +0,0 @@
-C:\gnu\Ruby24-x64\bin\setrbvars.cmd & bundle exec jekyll serve --incremental --config _config.yml,_config_dev.yml
--- zserve.sh
+++
@@ -1,3 +0,0 @@
-export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
-export PATH="$PATH:$GEM_HOME/bin"
-bundle exec jekyll serve --incremental --config _config.yml,_config_dev.yml --force_polling
